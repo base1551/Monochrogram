5676c1ba65572b51d9334a702f5ff58c5d2a7109
base1551 <baseball.1.kyoto@gmail.com>
2019-11-17 18:13:54 +0900

    system/user_logins実装

---
+++ spec/system/user_logins_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "UserLogins", type: :system do
+  # 登録すみユーザー作成
+  let(:user) { FactoryBot.create :user }
+  # ログイン画面
+  before do
+    visit new_user_session_path
+  end
+
+  context '有効な情報を送信したとき' do
+    before do
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: user.password
+      click_button 'ログイン'
+    end
+
+    it 'ログインに成功すること' do
+      expect(page).to have_content 'ログインしました。'
+    end
+  end
+
+  context '無効な情報が送信されたとき' do
+    before do
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード', with: 'user.password'
+      click_button 'ログイン'
+    end
+
+    it 'ログインに失敗すること' do
+      expect(page).to have_button 'ログイン'
+    end
+  end
+end
--- spec/system/users_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Users", type: :system do
-  before do
-    visit new_user_registration_path
-  end
-
-  describe
-end
