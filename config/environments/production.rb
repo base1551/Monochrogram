beddddad38293c541da6214f17dfe7d2e6f30d85
base1551 <baseball.1.kyoto@gmail.com>
2019-11-23 18:24:06 +0900

    title変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,10 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Karaage_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Monochrogram_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
