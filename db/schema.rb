fe04e93d2b394ac54c36e7ce8defe6bbfb2f1782
base1551 <baseball.1.kyoto@gmail.com>
2019-12-01 10:51:30 +0900

    db設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_105036) do
+ActiveRecord::Schema.define(version: 2019_11_30_083047) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_10_05_105036) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "profile_photo"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
